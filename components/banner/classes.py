1325d703068ccdf3d02f53acc63dc54b95274a78
experiencei <ayelojahighbee01@gmail.com>
2022-02-10 15:56:13 +0100

    working on the responsiveness of the websites

--- components/banner/classes.py
+++ components/banner/classes.py
@@ -1,15 +1,42 @@
-class PlayerCharacter:
-    # classs object attribute
-    membership = True
-    def __init__(self, name , gun):
-        self.name = name
-        self.gun = gun
+# class PlayerCharacter:
+#     # classs object attribute
+#     membership = True
+#     def __init__(self, name , gun):
+#         self.name = name
+#         self.gun = gun
 
-    def run(self):
-        print("Hello")
-        return "done"
+#     def run(self):
+#         print("Hello")
+#         return "done"
 
-player1 = PlayerCharacter("lukshark" , 12)
-player1.attack = 198
-print(player1.name)
-print(player1.attack)
+# player1 = PlayerCharacter("lukshark" , 12)
+# player1.attack = 198
+# print(player1.name)
+# print(player1.attack)
+
+
+# write a function that return the highest number
+arrayI = [ 1 , 4 , 5 , 8 , 10] 
+arrayI.sort()
+arrayI(-1)
+
+  # return  and print the even number in the List
+
+# arrayII = [ 9 , 13 , 45 , 12 , 56]
+# arrayII(3)
+# arrayII(4)
+
+
+
+   #sum the list to the total number
+
+# arrayIII = [ 1 , 4 , 6 , 890 , 67]
+# arrayIII.SUM()
+
+arrayII = [ 9 , 13 , 45 , 12 , 56]
+#  for item in arrayII :
+#      if item % 2 == 0 :
+#      print(item , end =" ")
+for item in arrayII :
+    if item % 2 == 0 :
+        print(item , end ="")
